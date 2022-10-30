72189f07a3272bb27f93ec5a7f88e741962f55dc
Soledad Beccaria <mariasoledadbeccaria@gmail.com>
2022-10-30 17:03:32 -0300

    ciclo

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-let peso = parseFloat(prompt("Ingresa tu peso en Kg"))
+    let peso = parseFloat(prompt("Ingresa tu peso en Kg"))
     let altura = parseFloat (prompt("Ingresa tu altura en Mt con . en lugar de ,"))
     let resultado =(peso/(altura*altura))
     console.log(resultado)
     
-    let IMC= resultado
-    if (IMC < 18.5) { 
+    let imc= resultado
+    do{
+    if (imc < 18.5) { 
        console.log("Tu peso está por debajo del promedio saludable");
-     } else if ((IMC  > 18.5) && (IMC < 24.9)) {
+     } else if ((imc  > 18.5) && (imc < 24.9)) {
         console.log("Tu peso es saludable"); 
-    } else if ((IMC  > 25)  && (IMC  < 29.9)) { 
+    } else if ((imc  > 25)  && (imc  < 29.9)) { 
         console.log("Tienes sobrepeso");
-    } else if ((IMC  > 30)  && (IMC  < 39.9)) {
+    } else if ((imc  > 30)  && (imc  < 39.9)) {
         console.log("Tu peso se considera obesidad. recomendamos consultar con un profesional de la salud");
-    } else if(IMC  > 40) { 
+    } else if(imc  > 40) { 
         console.log("Recomendamos consultar con un profesional de la salud");
-    }
+    } else if (imc!= undefined){
+        console.log("Introduce un valor válido")
+    } 
+} while (imc!= undefined){
+    
+}
+   
+
+    
     
