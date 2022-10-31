eceef5d8276c578e964d319b8f81f60bc4676215
Soledad Beccaria <mariasoledadbeccaria@gmail.com>
2022-10-31 14:39:25 -0300

    while

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+    
+    do{
     let peso = parseFloat(prompt("Ingresa tu peso en Kg"))
     let altura = parseFloat (prompt("Ingresa tu altura en Mt con . en lugar de ,"))
-    let resultado =(peso/(altura*altura))
-    console.log(resultado)
+    let imc =(peso/(altura**2))
+    console.log(imc)
     
-    let imc= resultado
-    do{
     if (imc < 18.5) { 
        console.log("Tu peso está por debajo del promedio saludable");
      } else if ((imc  > 18.5) && (imc < 24.9)) {
@@ -15,13 +15,9 @@
         console.log("Tu peso se considera obesidad. recomendamos consultar con un profesional de la salud");
     } else if(imc  > 40) { 
         console.log("Recomendamos consultar con un profesional de la salud");
-    } else if (imc!= undefined){
+    } else{
         console.log("Introduce un valor válido")
     } 
-} while (imc!= undefined){
-    
-}
+} while(imc=isNaN)
+console.log(imc)
    
-
-    
-    
